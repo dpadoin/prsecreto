9483026f13b2d21f681b196192cc332623560af9
dpadoin <dpadoin@gmail.com>
2024-12-03 19:48:40 -0300

    25

--- psecreto/settings.py
+++ psecreto/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import json
+import dj_database_url
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
