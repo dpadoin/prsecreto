5c9c4504d1cb84fad0872504f73e2a94c38e3270
dpadoin <dpadoin@gmail.com>
2024-12-03 12:59:13 -0300

    38

--- psecreto/settings.py
+++ psecreto/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "psecreto.apps.SecretoConfig",
+    "apps.SecretoConfig",
 ]
 
 MIDDLEWARE = [
